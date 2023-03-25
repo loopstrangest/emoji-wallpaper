31cf789b3006a563431a174aaedd40cb8e6b120b
loopstrangest <61626105+loopstrangest@users.noreply.github.com>
2023-03-25 03:48:42 -0500

    chore: revert mobile download

--- src/functions/Export.js
+++ src/functions/Export.js
@@ -1,18 +1,17 @@
-import html2canvas from 'html2canvas';
+import html2canvas from "html2canvas";
 const downloadScaleFactor = 2;
 
-
 export function downloadWallpaper() {
-  console.log('in downloadWallpaper');
+  console.log("in downloadWallpaper");
   const box = document.getElementById("emoji-wallpaper");
 
   // Temporarily add the no-transform class to the element
-  box.classList.add('no-transform');
+  box.classList.add("no-transform");
 
   // Capture the element using html2canvas
-  html2canvas(box, { useCORS: true }).then(canvas => {
+  html2canvas(box, { useCORS: true }).then((canvas) => {
     // Remove the no-transform class from the element
-    box.classList.remove('no-transform');
+    box.classList.remove("no-transform");
 
     // Create a new canvas with half the dimensions
     const newCanvas = document.createElement("canvas");
@@ -22,27 +21,16 @@ export function downloadWallpaper() {
     // Draw the scaled image on the new canvas
     const ctx = newCanvas.getContext("2d");
     ctx.drawImage(canvas, 0, 0, newCanvas.width, newCanvas.height);
-
-    // Use the new canvas to generate the data URL for download
-    const dataUrl = newCanvas.toDataURL('image/png');
+    const dataUrl = newCanvas.toDataURL("image/png");
+    
     const link = document.createElement("a");
     link.download = "emoji-wallpaper.png";
     link.href = dataUrl;
-
-    // Check if the browser is running on a mobile device
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-    if (isMobile) {
-      // Mobile device
-      window.open(dataUrl, '_blank', 'noopener noreferrer');
-    } else {
-      // Desktop device
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   });
-};
+}
 
 /*
 export function downloadWallpaper() {
